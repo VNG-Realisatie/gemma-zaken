9b79c2ec1a9915565f619605475aaf019c09baed
Rutger ter Borg <rutger.terborg@vng.nl>
2025-04-16 19:28:18 +0000

    chore: zgw-cleaner: naming conventions

--- tools/zgw_cleaner/src/zgw_cleaner/cleaners/naming_conventions.py
+++ tools/zgw_cleaner/src/zgw_cleaner/cleaners/naming_conventions.py
@@ -19,8 +19,6 @@ class NamingConventionsCleaner(Cleaner):
     def _pascal_case(self, name: str) -> str:
         return ''.join(word.capitalize() for word in name.split('_'))
 
-
-
     def clean(self, spec: Dict[str, Any], root_spec: Dict[str, Any] = None, path: list = []) -> Dict[str, Any]:
         """Cleans the OpenAPI spec by renaming fields."""
 
@@ -28,10 +26,15 @@ class NamingConventionsCleaner(Cleaner):
             root_spec = spec
 
         if path == ['components', 'schemas']:
+
             rename_to_pascalcase = []
             rename_to_baseclass = []
+            last_item_to_front_pascalcase = []
+            remove_object_prefix = []
+            search_replace_list = []
 
             for key, value in spec.items():
+
                 if camelcase(key) == key:
                     rename_to_pascalcase.append(key)
 
@@ -40,11 +43,40 @@ class NamingConventionsCleaner(Cleaner):
                     if base_name in spec:
                         rename_to_baseclass.append(base_name)
 
+                if key.startswith('Object') and not key.endswith('Base') and not key.endswith('Variant') and key != 'ObjectTypeEnum':
+                    if key.endswith('Enum'):
+                        remove_object_prefix.append(key)
+                    else:
+                        last_item_to_front_pascalcase.append(key)
+
+                if key.endswith('_PatchedZaakObject'):
+                    concept_snake = key[:-len('_PatchedZaakObject')]
+                    concept_pascal = pascalcase(concept_snake)
+                    new_key = 'PatchedZaak' + concept_pascal + 'Object'
+                    search_replace_list.append([key,new_key])
+
+                if key.endswith('_ZaakObject'):
+                    concept_snake = key[:-len('_ZaakObject')]
+                    concept_pascal = pascalcase(concept_snake)
+                    new_key = 'Zaak' + concept_pascal + 'Object'
+                    search_replace_list.append([key,new_key])
+
             for key in rename_to_pascalcase:
                 root_spec = self._rename_component(key, pascalcase(key), root_spec)
 
             for key in rename_to_baseclass:
                 root_spec = self._rename_component(key, key + 'Base', root_spec)
+
+            for key in last_item_to_front_pascalcase:
+                new_name = key[len('Object'):] + 'Object'
+                root_spec = self._rename_component(key, new_name, root_spec)
+
+            for key in remove_object_prefix:
+                new_name = key[len('Object'):]
+                root_spec = self._rename_component(key, new_name, root_spec)
+
+            for kv in search_replace_list:
+                root_spec = self._rename_component(kv[0], kv[1], root_spec)
 
         if isinstance(spec, dict):
             for old_name, new_name in self.field_name_mapping.items():
