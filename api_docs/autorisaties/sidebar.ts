8d41c3cdcc1fa36f50a9111fc031f4dee32b8fd9
Rutger ter Borg <rutger.terborg@vng.nl>
2025-09-03 09:51:50 +0000

    experimental api docs setup

---
+++ api_docs/autorisaties/sidebar.ts
@@ -0,0 +1,64 @@
+import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
+
+const sidebar: SidebarsConfig = {
+  apisidebar: [
+    {
+      type: "doc",
+      id: "autorisaties/autorisaties-api",
+    },
+    {
+      type: "category",
+      label: "applicaties",
+      link: {
+        type: "doc",
+        id: "autorisaties/applicaties",
+      },
+      items: [
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-list",
+          label: "Geef een collectie van applicaties, met ingesloten autorisaties.",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-create",
+          label: "Registreer een applicatie met een bepaalde set van autorisaties.",
+          className: "api-method post",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-consumer",
+          label: "Vraag een applicatie op, op basis van clientId",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-read",
+          label: "Vraag een applicatie op, met ingesloten autorisaties.",
+          className: "api-method get",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-update",
+          label: "Werk de applicatie bij.",
+          className: "api-method put",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-partial-update",
+          label: "Werk (een deel van) de applicatie bij.",
+          className: "api-method patch",
+        },
+        {
+          type: "doc",
+          id: "autorisaties/applicatie-delete",
+          label: "Verwijder een applicatie met de bijhorende autorisaties.",
+          className: "api-method delete",
+        },
+      ],
+    },
+  ],
+};
+
+export default sidebar.apisidebar;
---
+++ api_versioned_docs/version-1.5.1/intro.md
@@ -0,0 +1,32 @@
+---
+sidebar_position: 1
+tags:
+  - test
+---
+
+# Tutorial Intro
+
+Dit is hallo vanuit de API's.
+
+```mermaid
+   graph TD;
+        A-->B;
+        A-->C;
+        B-->D;
+        C-->D;
+
+```
+
+```mermaid
+  sequenceDiagram
+        participant Alice
+        participant Bob
+        Alice->>John: Hello John, how are you?
+        loop Healthcheck
+            John->>John: Fight against hypochondria
+        end
+        Note right of John: Rational thoughts <br/>prevail!
+        John-->>Alice: Great!
+        John->>Bob: How about you
+
+```
