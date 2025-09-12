7d214af4428a37b3e147cedf9af65fd9eb4162e7
Rutger ter Borg <rutger.terborg@vng.nl>
2025-09-12 10:23:33 +0000

    fixed broken link

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/gids">
+          <Link className="button button--secondary button--lg" to="/v1/gids">
             Ontdek ZGW - 5min ⏱️
           </Link>
         </div>
